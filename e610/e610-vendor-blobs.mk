6e625766e4aa1f64b07e76ecc7621fba82d2be23
RonGokhale <cips173@gmail.com>
2013-09-10 10:44:31 +0530

    e610: Fix BLuetooth
    Change-Id: I997dfd7db236b3dde26ad4327186e2ac7cd97df8

--- e610/e610-vendor-blobs.mk
+++ e610/e610-vendor-blobs.mk
@@ -20,7 +20,9 @@
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ######################    DEPENDENCY SECTION  #######################
 PRODUCT_COPY_FILES += \
-    vendor/lge/e610/proprietary/lib/libaudcal.so:obj/lib/libaudcal.so
+    vendor/lge/e610/proprietary/lib/libaudcal.so:obj/lib/libaudcal.so \
+    vendor/lge/e610/proprietary/lib/liboncrpc.so:obj/lib/liboncrpc.so \
+    vendor/lge/e610/proprietary/lib/libnv.so:obj/lib/libnv.so
 
 PRODUCT_COPY_FILES += \
     vendor/lge/e610/proprietary/lib/hw/lights.msm7x27a.so:system/lib/hw/lights.msm7x27a.so \
