0ea4650a33db68b4a4fcb8d924cb85579f976d82
RonGokhale <cips173@gmail.com>
2014-08-11 15:36:55 +0530

    msm7x27a-common: use adreno blobs from u.s s4 4.2 stock rom

--- msm7x27a-common/msm7x27a-common-vendor-blobs.mk
+++ msm7x27a-common/msm7x27a-common-vendor-blobs.mk
@@ -17,17 +17,15 @@
 PRODUCT_COPY_FILES += \
     vendor/lge/msm7x27a-common/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
     vendor/lge/msm7x27a-common/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/libsc-a2xx.so:system/vendor/lib/libsc-a2xx.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/egl/libGLESv2S3D_adreno.so:system/vendor/lib/egl/libGLESv2S3D_adreno.so \
-    vendor/lge/msm7x27a-common/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libEGL_adreno200.so:system/ib/egl/libEGL_adreno200.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    vendor/lge/msm7x27a-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
     vendor/lge/msm7x27a-common/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so
